8f0f180e0eafad6dc7a85a65d749ce00a8ee5504
Min-ho Kim <xho1995@gmail.com>
2016-04-01 12:37:48 +0900

    Initial Commit

---
+++ ModuLabsTests/ModuLabsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ModuLabsTests.swift
+//  ModuLabsTests
+//
+//  Created by Kim, Min Ho on 2016. 4. 1..
+//  Copyright © 2016년 Void Systems. All rights reserved.
+//
+
+import XCTest
+@testable import ModuLabs
+
+class ModuLabsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
