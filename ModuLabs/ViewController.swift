b92345ec49b9d467738ca7ceeee26a1c3c911d51
Mariokim <mario.kim@helloway.co>
2016-04-01 20:38:33 +0900

    print

--- ModuLabs/ViewController.swift
+++ ModuLabs/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hello")
+        print("hell")
     }
     
     override func viewDidAppear(animated: Bool) {
