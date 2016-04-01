54f66d510ca2cc66fa607be9b0cb2bee3bcd64e3
Min-ho Kim <xho1995@gmail.com>
2016-04-01 12:43:44 +0900

    Modify: app icon and view controller file

--- ModuLabs/ViewController.swift
+++ ModuLabs/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Properties
+    
+    //    var holderView = HolderView(frame: CGRectZero)
+    
+    // MARK: - Main Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        addHolderView()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - Helper Methods
+    
+    func addHolderView() {
+        //        holderView.frame = view.frame
+        //        holderView.frame = CGRect(x: view.bounds.width / 2 - boxSize / 2, y: view.bounds.height / 2 - boxSize / 2, width: boxSize, height: boxSize)
+        
+        let holderView = HolderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        //        holderView.parentFrame = view.frame
+        
+        view.addSubview(holderView)
+        
+        holderView.start()
+    }
+    
 }
 
